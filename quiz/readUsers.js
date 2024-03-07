ecd675a03d3ea435bde236bfe0d07c56eaa84865
Rahul Suresh <rahs98@gmail.com>
2024-03-06 19:30:19 -0500

    Class work

--- quiz/readUsers.js
+++ quiz/readUsers.js
@@ -1,2 +1,8 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
+
+router.get("/users", (req, res) => {
+    res.json({ users: req.users });
+});
+
+module.exports = router;
--- quiz/writeUsers.js
+++ quiz/writeUsers.js
@@ -1,4 +1,21 @@
-const express = require('express')
-const fs = require('fs');
-const path = require('path');
-const router = express.Router()
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const router = express.Router();
+
+router.post("/users", (req, res) => {
+    const newUser = req.body;
+    if (newUser) {
+        req.users.push(newUser);
+        res.status(201).json({
+            message: "User added successfully",
+            user: newUser,
+        });
+    } else {
+        res.status(400).json({
+            error: { message: "Invalid user data", status: 400 },
+        });
+    }
+});
+
+module.exports = router;
